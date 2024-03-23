300bb8813ae6ebde2c10909715d156b80547f806
P4ul0H <71473851+P4ul0H@users.noreply.github.com>
2024-03-23 01:17:56 -0300

    first

---
+++ src/test/java/io/github/p4ul0h0/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.p4ul0h0.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
